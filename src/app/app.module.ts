9591e2a00af8bee8a59d1c1c244fce1e65065dab
incheolha <incheolha@gmail.com>
2020-10-20 03:29:19 -0600

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ResumeComponent } from './welcome/resume/resume.component';
+import { HeaderComponent } from './welcome/header/header.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { LoginComponent } from './authentification/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumeComponent,
+    HeaderComponent,
+    AuthentificationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
